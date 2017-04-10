61aeaddc05883848ee4e208c456cd99d938570f5
Dmitriy.Litovchenko <Dmitriy.Litovchenko@yamalspg.ru>
2017-04-10 16:16:23 +0300

    +auth

--- web/main.dart
+++ web/main.dart
@@ -3,12 +3,12 @@ import 'dart:core';
 import 'package:angular2/platform/browser.dart';
 import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
-//import 'package:angular2/src/platform/browser/location/hash_location_strategy.dart';
 import 'package:angular2/platform/common.dart';
 
 import 'package:http/http.dart';
 import 'package:http/browser_client.dart';
 
+import 'package:auth/auth_service.dart';
 import 'package:config/config_service.dart';
 import 'package:alert/alert_service.dart';
 import 'package:master_layout/master_layout_component.dart';
@@ -36,7 +36,7 @@ main() async {
     ROUTER_PROVIDERS,
     const Provider(LocationStrategy, useClass: HashLocationStrategy),
     const Provider(AlertService),
-    const Provider(AsideService),
+    const Provider(AuthenticationService),
     const Provider(LoggerService),
     const Provider(ConfigService),
     const Provider(AsideService),
