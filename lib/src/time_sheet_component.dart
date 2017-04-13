98c3a25e2b6bbbc3c8476192ce4e7921d51b91d2
Dmitriy.Litovchenko <Dmitriy.Litovchenko@yamalspg.ru>
2017-04-13 11:13:02 +0300

    CMAS-136 Разработать frontend компонент таблицы Time sheet

--- lib/src/time_sheet_component.dart
+++ lib/src/time_sheet_component.dart
@@ -1,9 +1,47 @@
+import 'dart:html';
+
 import 'package:angular2/angular2.dart';
 import 'package:angular2/core.dart';
 
+import 'pipes/cm_week_day_pipe.dart';
+import 'time_sheet_rate_component.dart';
+
 @Component(
   selector: 'time-sheet',
-  templateUrl: 'time_sheet_component.html')
+  templateUrl: 'time_sheet_component.html',
+  directives: const [TimeSheetRateComponent],
+  pipes: const [
+    CmWeekDayPipe
+  ])
 class TimeSheetComponent {
+  List<DateTime> dates = new List<DateTime>();
 
+  TimeSheetComponent() {
+    // Первоначальная установка даты
+    DateTime now = new DateTime.now();
+
+    for (int dayIndex = 1; dayIndex <= 31; ++dayIndex) {
+      dates.add(new DateTime(now.year, now.month, dayIndex));
+    }
+  }
+
+  /**
+   * Выбор даты из списка
+   */
+  void selectMonth(SelectElement date) {
+    List<String> monthAndYear = date.value.split('.');
+
+    int month = int.parse(monthAndYear.first, onError: (_) => 0);
+    int year = int.parse(monthAndYear.last, onError: (_) => 0);
+
+    DateTime newDate = new DateTime(year, month, 1);
+
+    List<DateTime> tempDates = new List<DateTime>();
+
+    for (int dayIndex = 1; dayIndex <= 31; ++dayIndex) {
+      tempDates.add(new DateTime(newDate.year, newDate.month, dayIndex));
+    }
+
+    dates = tempDates;
+  }
 }
---
+++ lib/src/time_sheet_rate_component.dart
@@ -0,0 +1,9 @@
+import 'package:angular2/angular2.dart';
+import 'package:angular2/core.dart';
+
+@Component(
+  selector: 'time-sheet-rate',
+  templateUrl: 'time_sheet_rate_component.html')
+class TimeSheetRateComponent {
+
+}
